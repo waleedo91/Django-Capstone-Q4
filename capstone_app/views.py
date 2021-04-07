d648e9d88968f2507c14fcc407896be83b834817
Jonathan Sueck <jonathansueck@gmail.com>
2021-04-07 14:11:59 -0400

    game is displaying ate /game/1/

--- capstone_app/views.py
+++ capstone_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Game
 
 # Create your views here.
+def gameview(request, game_id):
+    game = Game.objects.get(id=game_id)
+    return render(request, 'game.html', {'game': game})
+
