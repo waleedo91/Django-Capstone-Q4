8f2c07bdd45311fed08636c642a9557d7a117e62
Waleed Saleh <wsaleh1991@gmail.com>
2021-04-07 13:00:52 -0500

    added comment on gamereview form

--- capstone_app/forms.py
+++ capstone_app/forms.py
@@ -3,6 +3,10 @@ from django.forms.widgets import Textarea
 
 from .models import GameGenre, Game, Player, GameReview
 
+'''Going to give the game variable a test to make sure we are able to choose from listed games and choose from that list.
+
+-Waleed'''
+
 
 class GameReviewForm(forms.Form):
     game = forms.ModelsChoiceField(queryset=Game.objects.all())
