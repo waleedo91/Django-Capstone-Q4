917c97143e32cea6e32082bc44858b627b0151d6
Jonathan Sueck <jonathansueck@gmail.com>
2021-04-07 17:43:35 -0400

    notes added: get each game page information from an api call

--- capstone_app/views.py
+++ capstone_app/views.py
@@ -3,6 +3,10 @@ from .models import Game
 
 # Create your views here.
 def gameview(request, game_id):
+    # game id = api_req(id)
+    # check to see if game data exists
     game = Game.objects.get(id=game_id)
     return render(request, 'game.html', {'game': game})
+    # else
+    # Search API for game and add to database
 
