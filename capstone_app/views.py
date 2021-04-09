ceb368f9bc88a56c4b1c391a1f6ec0732fd6a362
Jonathan Sueck <jonathansueck@gmail.com>
2021-04-09 10:27:48 -0400

    singular game display is displaying name at game/id

--- capstone_app/views.py
+++ capstone_app/views.py
@@ -8,7 +8,7 @@ max_num = 5
 gamesList = dict()
 exit_flag = False
 def create_games(request):
-    i = 1
+    i = 257
     while not exit_flag:
         try:
             url = f"https://rawg-video-games-database.p.rapidapi.com/games/{ i }"
@@ -27,7 +27,7 @@ def create_games(request):
 def gameview(request, game_id):
     # game id = api_req(id)
     # check to see if game data exists
-    game = Game.objects.get(id=game_id)
+    game = Game.objects.get(game_id=game_id)
     return render(request, 'game.html', {'game': game})
     # else
     # Search API for game and add to database
