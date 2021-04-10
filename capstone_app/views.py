0cd9cf98f8028cd4c533dd440dbc20a4e4fb8f75
Jonathan Sueck <jonathansueck@gmail.com>
2021-04-10 17:32:22 -0400

    added in note for fn

--- capstone_app/views.py
+++ capstone_app/views.py
@@ -5,22 +5,10 @@ import requests
 import json
 
 # Create your views here.
-gamesList = dict()
-# exit_flag = False
-def create_games(request):
-    url = 'https://api.rawg.io/api/platforms/lists/parents?key=1d0a743d255d48418ee551a3eb563813&page_size=100&platforms=18&language="eng"'
-    playstation_4_games = 'https://api.rawg.io/api/games?key=1d0a743d255d48418ee551a3eb563813&dates=2019-01-01,2019-12-31&ordering=-added&language=en'
-    response = requests.request("GET", playstation_4_games)
-    res_dict = response.json()
-    return render(request, 'index.html', {'dict': res_dict})
-
+# gets game id from url and returns game information
 def gameview(request, game_id):
     url = f'https://api.rawg.io/api/games/{ game_id }?key=1d0a743d255d48418ee551a3eb563813'
     game = requests.request("GET", url)
     resp = game.json()
     return render(request, 'game.html', {'game': resp})
-
-def games(request):
-    games = Game.objects.all()
-    return render(request, 'index.html', {'games': games})
 
