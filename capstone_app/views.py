6bd0273ccbf8e460c30c350349558c02086c1731
Jonathan Sueck <jonathansueck@gmail.com>
2021-04-10 17:17:20 -0400

    displaying game information for dirt4 on game detail page. now working on dynamically changing number for all games

--- capstone_app/views.py
+++ capstone_app/views.py
@@ -2,31 +2,26 @@ from django.http.response import Http404
 from django.shortcuts import render
 from .models import Game
 import requests
+import json
 
 # Create your views here.
-max_num = 5
 gamesList = dict()
-exit_flag = False
+# exit_flag = False
 def create_games(request):
-    i = 257
-    while not exit_flag:
-        try:
-            url = f"https://rawg-video-games-database.p.rapidapi.com/games/{ i }"
-            headers = {
-                'x-rapidapi-key': "4329ef7408msh53be98e03ed8801p11ea0djsn9d388763b4fa",
-                'x-rapidapi-host': "rawg-video-games-database.p.rapidapi.com"
-            }
-            response = requests.request("GET", url, headers=headers)
-            resp = response.json()
-            Game.objects.create(name=resp['name'], game_id=resp['id'])
-            i += 1
-            print('game added')
-        except ConnectionRefusedError:
-            break
+    url = 'https://api.rawg.io/api/platforms/lists/parents?key=1d0a743d255d48418ee551a3eb563813&page_size=100&platforms=18&language="eng"'
+    playstation_4_games = 'https://api.rawg.io/api/games?key=1d0a743d255d48418ee551a3eb563813&dates=2019-01-01,2019-12-31&ordering=-added&language=en'
+    response = requests.request("GET", playstation_4_games)
+    res_dict = response.json()
+    print(res_dict)
+    return render(request, 'index.html', {'dict': res_dict})
 
 def gameview(request, game_id):
-    game = Game.objects.get(game_id=game_id)
-    return render(request, 'game.html', {'game': game})
+    game_id = 21
+    url = f'https://api.rawg.io/api/games/{ game_id }?key=1d0a743d255d48418ee551a3eb563813'
+    game = requests.request("GET", url)
+    resp = game.json()
+    print(resp)
+    return render(request, 'game.html', {'game': resp})
 
 def games(request):
     games = Game.objects.all()
