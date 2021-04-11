38db71514147ae7b2edd4c19eb88efc0e3545f4e
Darian Martinez <darian.mtz18@gmail.com>
2021-04-11 17:34:03 -0500

    added the s

--- capstone_django/urls.py
+++ capstone_django/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('player/<int:user_id>/', playerview, name="playerview"),
-    path('review/', ReviewsView.as_view(), name='review')
+    path('reviews/', ReviewsView.as_view(), name='review')
 ]
