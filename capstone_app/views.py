5e8036e47b98815e5b4119ebc4c52722897dbb11
Waleed Saleh <wsaleh1991@gmail.com>
2021-04-11 17:51:10 -0500

    working on background for splash page

--- capstone_app/views.py
+++ capstone_app/views.py
@@ -1,12 +1,20 @@
+from django.http import response
 from django.shortcuts import render
 import requests
+from capstone_django.settings import DATABASES
 
 from .models import Game
 # Create your views here.
 
+# url = f"https://api.rawg.io/api/games?key={{DATABASES.API}}"
+
 
 def index(request):
-    game = Game.objects.all()
+    url = "https://api.rawg.io/api/games?key=1d0a743d255d48418ee551a3eb563813&metacritic=%2295,100%22&page_size=20"
+    game = requests.request("GET", url)
+    resp = game.json()
     return render(request, 'index.html', {
-        'game': game
+        'games': resp
     })
+
+
