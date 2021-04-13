8ab4d362506611a0126d82e2e6d3f841d44ebd70
Waleed Saleh <wsaleh1991@gmail.com>
2021-04-13 15:36:39 -0500

    created new branch for 404 and 500 pages

--- capstone_django/settings.py
+++ capstone_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_t&jw*(5&50zc7d9uuul^o!!ndyo!r^vkm$$!sny!fro74+r06'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
