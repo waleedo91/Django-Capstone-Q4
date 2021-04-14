a90c9f965bb50635a3f8d7891165b7fa435cbb14
Waleed Saleh <wsaleh1991@gmail.com>
2021-04-14 13:58:09 -0500

    Switched Debug to True.

--- capstone_django/settings.py
+++ capstone_django/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '_t&jw*(5&50zc7d9uuul^o!!ndyo!r^vkm$$!sny!fro74+r06'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
